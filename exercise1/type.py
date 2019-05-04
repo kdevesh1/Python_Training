b26a9c4c3f1e4260bd3ddfdcfe1df1f8640f7b8a
kdevesh1 <devesh.kumar@vvdntech.in>
2019-05-05 00:36:14 +0530

    convert to different Data type and print

---
+++ exercise1/type.py
@@ -0,0 +1,24 @@
+#  Get the input from user and convert it into different data types and print it
+
+num=int(input("enter any number"))
+print(num)
+print(type(num))
+
+fnum=float(input("enter any number"))
+print(fnum)
+print(type(fnum))
+
+#lnum=long(input("enter any number"))
+#print(fnum)
+#print(type(fnum))
+
+Cnum=complex(input("enter any number"))
+print(Cnum)
+print(type(Cnum))
+
+string=str(input("enter any string or msg :"))
+print(string)
+print(type(string))
+
+
+
