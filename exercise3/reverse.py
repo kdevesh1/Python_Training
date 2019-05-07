fd2765caa298c1ff4fae5160e61d2553c235d325
kdevesh1 <devesh.kumar@vvdntech.in>
2019-05-07 14:40:46 +0530

    reverse the input from user

--- exercise3/reverse.py
+++ exercise3/reverse.py
@@ -1,7 +1,7 @@
-#  
+# Write the program to get the name of user and reverse it 
 
 word = input("Input a word to reverse: ")
 
 for ch in range(len(word) - 1, -1, -1):
   print(word[ch],end="")
-print("\n")
+
