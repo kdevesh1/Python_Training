5aedd17b2542e85042a160637bb9fff3ea10b1a1
kdevesh1 <devesh.kumar@vvdntech.in>
2019-05-26 21:37:06 +0530

    circle  area

--- exercise7/circle_area.py
+++ exercise7/circle_area.py
@@ -1,3 +1,6 @@
+# Write a Python class named Circle constructed by a radius and two methods which will compute the area and the perimeter of a circle.
+
+
 class Circle():
     def __init__(self, r):
         self.radius = r
